93987b2e8d918d9cda939d79fac35dd1f850518c
tofu2323 <gotch@styz.io>
2021-06-19 12:10:32 +0900

    fix to use myname

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Fumiya Goto',
+    site_url: 'https://gotch.work',
+    feed_url: 'https://gotch.work/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
